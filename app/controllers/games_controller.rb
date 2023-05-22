ebe73fd0232d1fdf061577cbfdbfbb9687d3c0ad
Etienne Leguay <etienneleguay@icloud.com>
2023-05-22 19:29:38 -0400

    Last update

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -17,11 +17,22 @@ class GamesController < ApplicationController
     @condition = @letter_array.all? {|letter| @letters.include?(letter) && @letter_array.count(letter) <= @letters.count(letter) }
     if hash["found"] && @condition
       @response = "Congrats, you won the game pal"
+      @score = @word.length**2
+      @response += "with a score of #{@score}"
     elsif @condition == false
       @response = "Sorry, but #{@word} can't be built out of #{@letters}"
+      @score = 0
     else
       @response = "Sorry, but #{@word} is not an english word"
+      @score = 0
     end
+    if session[:grand_score].nil?
+      session[:grand_score] = [@score]
+    else
+      session[:grand_score].push(@score)
+    end
+    @session_array = session[:grand_score]
+    @grand = @session_array.sum
   end
 
 end
